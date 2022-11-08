5e60815040800c1b692debad7a150bdb4dcd4f78
tskozel <torsten.kozel@seznam.cz>
2022-11-08 21:21:08 +0100

    ValidatedSelectionOrder & reacting on first selection & fixed option clearing

--- main.js
+++ main.js
@@ -16,6 +16,14 @@ function find_key(object, value) {
   return "undefined";
 }
 
+function right_side(seq, from) {
+  const lower = seq.indexOf(from);
+  if (lower === -1) {
+    return lower;
+  }
+  return seq.slice(lower+1, seq.length);
+}
+
 class SelectionOrder {
   constructor(shifts, setup=null) {
     this.shifts = shifts;
@@ -45,7 +53,13 @@ class SelectionOrder {
       this.shifts.select_elem[i].addEventListener("change", (event) => {
         this.on_selection(event.target, event.target.value);
       });
-    })
+    });
+    Object.values(this.shifts.select_elem).forEach((selbox) => {
+      let child = window.document.createElement("option");
+      child.selected = true;
+      child.className = "blank-opts-ds";
+      selbox.insertBefore(child, selbox.firstChild);
+    });
   }
 
   on_selection(selElem, sel) {
@@ -57,6 +71,9 @@ class SelectionOrder {
   }
 
   on_selection__default(selElem, sel) {
+    if (sel === "") {
+      return;
+    }
     const elem_name = find_key(this.shifts.select_elem, selElem);
     if (typeof this.shifts.events[elem_name] === "undefined") {
       throw Error(`Element "${elem_name}" doesn't have any events`);
@@ -64,16 +81,28 @@ class SelectionOrder {
     if (typeof this.shifts.events[elem_name][sel] === "undefined") {
       throw Error(`"${sel}" choice is not defined under element "${elem_name}"`);
     }
-
-    let child;
+    right_side(Object.keys(this.shifts.select_elem), elem_name).forEach((selbox_name) => {
+      this.shifts.select_elem[selbox_name].innerHTML = "";
+    });
+    let new_child;
     this.shifts.events[elem_name][sel].forEach((elem) => {
-      elem.innerHTML = "";
+      if (Object.prototype.toString.call(elem) === "[object Ok]") {
+        return; // May cause problems with adding to left-side selectboxes?
+      }
+      elem.sE.innerHTML = "";
+      new_child = window.document.createElement("option");
+      new_child.selected = true;
+      new_child.className = "blank-opts-ds";
+      elem.sE.insertBefore(new_child, elem.firstChild);
     });
     this.shifts.events[elem_name][sel].forEach((choice) => {
-      child = window.document.createElement("option");
-      child.value = choice.add;
-      child.innerText = choice.add;
-      choice.sE.appendChild(child);
+      if (Object.prototype.toString.call(choice) === "[object Ok]") {
+        return; // Same as line 90
+      }
+      new_child = window.document.createElement("option");
+      new_child.value = choice.add;
+      new_child.innerText = choice.add;
+      choice.sE.appendChild(new_child);
     });
   }
 
@@ -94,7 +123,58 @@ class Choice {
     this.sE = selElem;
     this.add = addition;
   }
+  get [Symbol.toStringTag]() {
+    return 'Choice';
+  }
 }
 
+class Ok {
+  constructor(func, destructor) {
+    this.func = func;
+    this.args = arguments;
+    this.destructor = destructor;
+  }
+  exec() {
+    // May cause problems if no args provided
+    this.func.apply(null, Array.from(this.args).slice(2));
+  }
+  get [Symbol.toStringTag]() {
+    return 'Ok';
+  }
+  get destr() {
+    return this.destructor;
+  }
+}
 
-module.exports = { SelectionOrder: SelectionOrder, Choice: Choice};
+class ValidatedSelectionOrder extends SelectionOrder {
+  constructor(shifts, submitter) {
+    super(shifts);
+    this.submitter = submitter;
+    this.okay = false;
+    this.handlerOverride = this.validated_on_selection__default;
+    this.submitter.hidden = true;
+  }
+  validated_on_selection__default(selElem, sel) {
+    this.okay = false;
+    const elem_name = find_key(this.shifts.select_elem, selElem);
+    this.shifts.events[elem_name][sel].forEach((member) => {
+       if (Object.prototype.toString.call(member) === "[object Ok]") {
+         this.okay = true;
+         member.exec();
+       }
+    });
+    this.on_selection__default(selElem, sel)
+    if (this.okay) {
+      this.submitter.hidden = false;
+    }
+  }
+}
+
+if (! (typeof module === "undefined")) {
+  module.exports = {
+    SelectionOrder: SelectionOrder,
+    Choice: Choice,
+    Ok: Ok,
+    ValidatedSelectionOrder: ValidatedSelectionOrder,
+  };
+}
